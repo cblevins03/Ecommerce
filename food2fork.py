bcd3238c8d39763b839aa95ebae3aebb3413f173
Carson Blevins <cblevins03@bellarmine.edu>
2017-03-24 02:28:17 -0400

    Add files via upload

--- food2fork.py
+++ food2fork.py
@@ -19,74 +19,92 @@ data = json.loads(r.text)
 #r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 #recipe_info = json.loads(r2.text)['recipe']		
 
-
 def f2furl(dish):
 	id = data['recipes'][dish]['recipe_id']
 	r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 	recipe_info = json.loads(r2.text)['recipe']
-	print(recipe_info['f2f_url'])
+	print('F2F URL: \n\t',recipe_info['f2f_url'])
+	print('\n')
 
 def sourceurl(dish):
 	id = data['recipes'][dish]['recipe_id']
 	r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 	recipe_info = json.loads(r2.text)['recipe']
-	print(recipe_info['source_url'])
+	print('Source URL: \n\t',recipe_info['source_url'])
+	print('\n')
 
 def publisher(dish):
 	id = data['recipes'][dish]['recipe_id']
 	r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 	recipe_info = json.loads(r2.text)['recipe']
-	print(recipe_info['publisher'])
-
+	print('Publisher: \n\t',recipe_info['publisher'])
+	print('\n')
+	
 def recipeid(dish):
 	id = data['recipes'][dish]['recipe_id']
 	r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 	recipe_info = json.loads(r2.text)['recipe']
-	print(recipe_info['recipe_id'])
-
+	print('Recipe ID: \n\t',recipe_info['recipe_id'])
+	print('\n')
+	
 def	title(dish):
 	id = data['recipes'][dish]['recipe_id']
 	r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 	recipe_info = json.loads(r2.text)['recipe']
-	print(recipe_info['title'])
-
+	print('Title: \n\t',recipe_info['title'])
+	print('\n')
+	
 def socialrank(dish):
 	id = data['recipes'][dish]['recipe_id']
 	r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 	recipe_info = json.loads(r2.text)['recipe']
-	print(recipe_info['social_rank'])
+	print('Social Rank: \n\t',recipe_info['social_rank'])
+	print('\n')
 	
 def publisherurl(dish):
 	id = data['recipes'][dish]['recipe_id']
 	r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 	recipe_info = json.loads(r2.text)['recipe']
-	print(recipe_info['publisher_url'])
-
+	print('Publisher URL: \n\t',recipe_info['publisher_url'])
+	print('\n')
+	
 def imageurl(dish):
 	id = data['recipes'][dish]['recipe_id']
 	r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 	recipe_info = json.loads(r2.text)['recipe']
-	print(recipe_info['image_url'])
-
+	print('Image URL: \n\t',recipe_info['image_url'])
+	print('\n')
+	
 def ingredients(dish):
 	id = data['recipes'][dish]['recipe_id']
 	r2 = requests.post(recipe_url, data={'key': key, 'rId': id})
 	recipe_info = json.loads(r2.text)['recipe']
+	print('Ingredients: ')
 	for ingr in recipe_info['ingredients']:
-		print(ingr)
-
+		print('\t',ingr)
+	print('\n')
 		
-dishChoice = 3
+print('\n')
+
+
+dishChoice = int(input('Choose dish:'))
+		
+#	Or you can hardcode it:
+#dishChoice = 1
+
+print('\n\n')
 
 title(dishChoice)
-recipeid(dishChoice)
-imageurl(dishChoice)
+#recipeid(dishChoice)
+#imageurl(dishChoice)
 ingredients(dishChoice)
 #f2furl(dishChoice)
-#sourceurl(dishChoice)
+sourceurl(dishChoice)
 #publisher(dishChoice)		
 #socialrank(dishChoice)		
 #publisherurl(dishChoice)
+
+print('\n')
 
 #print("Complete")
 
